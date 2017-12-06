56be9c0d167aa06ef0c237f5ac5a225f89a77002
kambli98 <33296235+kambli98@users.noreply.github.com>
2017-12-06 16:37:18 -0500

    Add files via upload

---
+++ 687281_HarshitKambli_CSD3184_FinalXamarin.zip/BlogPage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FinalProjectXamarin
+{
+    [Activity(Label = "Blog")]
+    public class BlogAuthorDescription : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.BlogPage);
+
+
+            Button showPopupMenu = FindViewById<Button>(Resource.Id.button1);
+
+            showPopupMenu.Click += delegate {
+
+
+                PopupMenu menu = new PopupMenu(this, showPopupMenu);
+                menu.Inflate(Resource.Menu.popup_menu);
+
+                menu.MenuItemClick += (s1, e1) =>
+                {
+                    switch (e1.Item.TitleFormatted.ToString())
+                    {
+                        case "Places Visited":
+                            StartActivity(typeof(List));
+                            break;
+                        case "Destination to be Visited":
+                            StartActivity(typeof(List2));
+                            break;
+                        case "Contact Us":
+                            StartActivity(typeof(contact));
+                            break;
+                    }
+                };
+                menu.Show();
+
+
+            };
+        }
+    }
+}
---
+++ 687281_HarshitKambli_CSD3184_FinalXamarin.zip/SplashActivity.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FinalProjectXamarin
+{
+    [Activity(Label = "FinalProjectXamarin", MainLauncher = true)]
+    public class splashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Splash);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        // Simulates background work that happens behind the splash screen
+        async void SimulateStartup()
+        {
+            await Task.Delay(8000); // Simulate a bit of startup work.
+            StartActivity(new Intent(Application.Context, typeof(BlogPage)));
+        }
+
+    }
+}
