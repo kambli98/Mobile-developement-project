1bacac4a2a7b1d4a562c1581db8b55d2c35a1174
kambli98 <33296235+kambli98@users.noreply.github.com>
2017-12-06 16:33:23 -0500

    Add files via upload

---
+++ Upload/HomeScreenAdapter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+
+namespace FinalProjectXamarin
+{
+    [Activity(Label = "Visited Places")]
+    public class myListView : Activity
+    {
+        List<TableItem> tableItems = new List<TableItem>();
+        ListView listView;
+        HomeScreenAdapter homeAdapter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.myListView);
+
+            listView = FindViewById<ListView>(Resource.Id.countryList);
+
+            tableItems.Add(new TableItem() { Heading = "Agra", SubHeading = "BEAUTY OF BEACHES", ImageResourceId = Resource.Drawable. agra.jpg});
+            tableItems.Add(new TableItem() { Heading = "Mumbai", SubHeading = "SUNSHINE ON ISLAND", ImageResourceId = Resource.Drawable.mumbai.jpg});
+            tableItems.Add(new TableItem() { Heading = "France", SubHeading = "CITY OF HEIGHT", ImageResourceId = Resource.Drawable.france.jpg });
+            tableItems.Add(new TableItem() { Heading = "London", SubHeading = "BOLLYWOOD", ImageResourceId = Resource.Drawable.London.jpg });
+            tableItems.Add(new TableItem() { Heading = "New York", SubHeading = "CITY OF LOVE", ImageResourceId = Resource.Drawable.NewYork.jpg });
+            homeAdapter = new HomeScreenAdapter(this, tableItems);
+
+            listView.Adapter = homeAdapter;
+            listView.ItemClick += OnListItemClick;
+        }
+
+        protected void OnListItemClick(object sender, Android.Widget.AdapterView.ItemClickEventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Upload/List2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+
+namespace FinalProjectXamarin
+{
+    [Activity(Label = "Destinations to be visited")]
+    public class myListView2 : Activity
+    {
+
+        static readonly string[] countries = new String[] {
+            "Delhi","Egypt","USA","Sweden","Rajastan"
+            };
+        ListView lstCountryList;
+        ArrayAdapter<string> countryDataAdapter;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            SetContentView(Resource.Layout.List2);
+
+            lstCountryList = FindViewById(Resource.Id.List) as ListView;
+
+            countryDataAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, countries);
+            lstCountryList.Adapter = countryDataAdapter;
+
+
+            lstCountryList.ItemClick += listView_ItemClick;
+        }
+
+        void listView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+
+            var item = this.countryDataAdapter.GetItem(e.Position);
+
+
+            Toast.MakeText(this, item + " Clicked!", ToastLength.Short).Show();
+        }
+
+    }
+}
